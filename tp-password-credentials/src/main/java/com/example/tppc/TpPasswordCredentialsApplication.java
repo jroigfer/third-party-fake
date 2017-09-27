fca10dcdb07e358976c70d55c00d62edd8b05111
josep <josep.joaquim.roig.ferrer@everis.com>
2017-09-27 17:33:33 +0200

    initial changes

---
+++ tp-password-credentials/src/main/java/com/example/tppc/TpPasswordCredentialsApplication.java
@@ -0,0 +1,12 @@
+package com.example.tppc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPasswordCredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPasswordCredentialsApplication.class, args);
+	}
+}
